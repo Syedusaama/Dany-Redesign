96225211e7af3a2de6958ad6f0562fc3aee3c8bc
Syed Usama <syedusama875@gmail.com>
2023-06-02 04:07:44 -0700

    update

--- index.php
+++ index.php
@@ -10,10 +10,129 @@
 
 <body>
     <?php include("includes/header.php"); ?>
-    <div class="mainBanner" style="background-image:url(assets/images/banner/bg-banner.jpg); ">
-        <div class="container">
+    <!-- main-banner -->
 
+    <section class="main-banner" style="background-image:url(assets/images/banner.png); ">
+        <div class="banner-box">
+            <div class="row row align-items-center">
+                <div class="col-md-6">
+                    <div class="banner-content">
+                <div class="sec-heading">
+                <p class="banner-p">Lorem ipsum dolor sit amet,</p>
+                <h2 class="banner-h2">Refer a Friend!</h2>
+            </div>
+            <div class="row align-items-center">
+                <div class="col-md-3">
+                <a href="#" class="main-btn">Refer a Friend</a>
+                </div>
+                <div class="col-md-9">
+                <p>Friends and family is what really matters in life, and DANY you can give your F'nF a little extra. With the DANY Referral program now</p>
+                </div>
+                </div> 
+            </div> 
         </div>
+                <div class="col-md-6">
+                    <img src="assets/images/banner-mobile.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- main-banner -->
+
+    <!-- main-banner -->
+    <section class="bg-section-1">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <img src="assets/images/second-sec.png" alt="">
+                </div>
+                <div class="col-md-6">
+                <div class="sec-heading">
+                    <h2>Join Our Referral<br><span style="color:#282828">Program</span></h2>
+                    <p>Friends and family is what really matters in life, and DANY you can give your F'nF a little extra. With the DANY Referral program now you can get exclusive discounts for all your loved ones; and in return, you get a gift as well. So start the circle of giving today and make your family and friends bonafide Audionic Stars!</p>
+                    <a href="#" class="blue-btn">Refer a Friend</a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- main-banner -->
+
+    <!-- main-banner -->
+    <section class="bg-section-2">
+        <div class="container">
+        <div class="sec-heading text-center">
+                    <h2>How the<br><span style="color:#282828"> Referral Program Works</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+                </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="assets/images/work.png" alt="">
+                </div>
+            </div>
+            <div class="row text-center align-items-center">
+                <!-- <div class="col-md-1"></div> -->
+                <div class="col-md-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo.</p>
+                </div>
+                <div class="col-md-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo.</p>
+                </div>
+                <div class="col-md-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo.</p>
+                </div>
+                <div class="col-md-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo.</p>
+                </div>
+                <div class="col-md-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo.</p>
+                </div>
+                <!-- <div class="col-md-1"></div> -->
+            </div>
+        </div>
+    </section>
+    <section class="bg-section-3">
+        <div class="container">
+            <div class="row">
+
+            </div>
+        </div>
+    </section>
+
+    <!-- main-banner -->
+
+    <!-- main-banner -->
+    <section class="bg-section-4">
+        <div class="container">
+            <div class="row">
+
+            </div>
+        </div>
+    </section>
+    <section class="bg-section-5">
+        <div class="container">
+            <div class="row">
+
+            </div>
+        </div>
+    </section>
+
+    <!-- main-banner -->
+
+    <!-- main-banner -->
+    <section class="bg-section-6">
+        <div class="container">
+            <div class="row">
+
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- main-banner -->    
     </div>
 
     <?php include("includes/footer.php"); ?>
